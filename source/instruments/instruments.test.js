25aab3dd310097d25392ce37670eeb2655df195b
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-16 09:56:22 +0300

    → Checkpoint at: 12:28:17

--- source/instruments/instruments.test.js
+++ source/instruments/instruments.test.js
@@ -4,4 +4,8 @@ describe('helpers:', () => {
     test('sum function should be a function', () => {
         expect(sum).toBeInstanceOf(Function);
     });
+
+    test('should throw if call with non number', () => {
+        expect(sum).toBeInstanceOf(Function);
+    });
 });
