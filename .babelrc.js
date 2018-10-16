5f4faec42d76fa77b029d16a1523c4af900c1e40
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-16 09:56:22 +0300

    → Checkpoint at: 12:15:38

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,7 @@ module.exports = api => {
     const env = api.env();
 
     api.cache.using(() => env === 'development');
+    api.cache.never();
 
     const plugins = [
         ['@babel/plugin-proposal-decorators', { legacy: true }],
