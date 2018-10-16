b8dccb3576a006495fcb76cbf58069953aeabdbf
Dima Vakatsiienko <d.vakatsiienko@lectrum.io>
2018-10-16 09:56:22 +0300

    → Checkpoint at: 10:38:00

--- source/pages/Root/index.js
+++ source/pages/Root/index.js
@@ -1,31 +1,147 @@
+/* eslint-disable react/no-multi-comp */
 // Core
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 
-// Components
-import Feed from '../../components/Feed';
-import Catcher from '../../components/Catcher';
-import { Provider } from '../../HOC';
-
-// Instruments
-import avatar from '../../theme/assets/homer';
-
-const options = {
-    avatar,
-    currentUserFirstName: 'Андрей',
-    currentUserLastName: 'Присняк',
+const log = (message, color) => {
+    return console.log(`%c ${message}`, `background: #222; color: ${color}`);
 };
 
-@hot(module)
-export default class Root extends Component {
-    render() {
+class Silo extends Component {
+    static getDerivedStateFromProps (nextProps, prevState) {
+        log('CHILD → static getDerivedStateFromProps', 'orange');
+
+        if (nextProps.apples > prevState.apples) {
+            return {
+                apples: nextProps.apples,
+            };
+        }
+
+        return null;
+    }
+
+    constructor () {
+        super();
+
+        log('CHILD → constructor', 'wheat');
+    }
+
+    state = {
+        apples:  0,
+        carrots: 5,
+    };
+
+    componentDidMount () {
+        log('CHILD → componentDidMount', 'lime');
+    }
+
+    shouldComponentUpdate () {
+        log('CHILD → shouldComponentUpdate', 'olive');
+
+        return true;
+    }
+
+    componentDidUpdate () {
+        log('CHILD → componentDidUpdate', 'yellow');
+    }
+
+    getSnapshotBeforeUpdate () {
+        log('CHILD → getSnapshotBeforeUpdate', 'coral');
+
+        return null;
+    }
+
+    _yieldCarrot = () =>
+        this.setState(({ carrots }) => ({ carrots: carrots + 1 }));
+
+    render () {
+        const { apples, carrots } = this.state;
+
+        log('CHILD → render', 'red');
+
+        const vegetables = Array(carrots).fill('🥕');
+        const fruits = Array(apples).fill('🍎');
+
+        const fruitsAndVegetablesList = [...vegetables, ...fruits];
+
         return (
             <>
-                <Catcher>
-                    <Provider value={options}>
-                        <Feed />
-                    </Provider>
-                </Catcher>
+                <br />
+                <br />
+                <br />
+                <br />
+                <h1>📦 Коробка с овощами и фруктами</h1>
+                <br />
+                <p>{fruitsAndVegetablesList}</p>
+                <br />
+                <button onClick = { this._yieldCarrot }>Собрать морковь 🥕</button>
+            </>
+        );
+    }
+}
+
+// Компонент-фермер, собирает урожай
+@hot(module)
+export default class Farmer extends Component {
+    static getDerivedStateFromProps () {
+        log('PARENT → static getDerivedStateFromProps', 'orange');
+
+        return null;
+    }
+
+    constructor () {
+        super();
+
+        log('PARENT → constructor', 'wheat');
+    }
+
+    state = {
+        apples: 5,
+        name:   'Уолтер',
+    };
+
+    componentDidMount () {
+        log('PARENT → componentDidMount', 'lime');
+    }
+
+    shouldComponentUpdate () {
+        log('PARENT → shouldComponentUpdate', 'olive');
+
+        return true;
+    }
+
+    componentDidUpdate () {
+        log('PARENT → componentDidUpdate', 'yellow');
+    }
+
+    getSnapshotBeforeUpdate () {
+        log('PARENT → getSnapshotBeforeUpdate', 'coral');
+
+        return null;
+    }
+
+    _yieldApples = () => {
+        this.setState(({ apples }) => ({ apples: apples + 1 }));
+    };
+
+    render () {
+        const { name, apples } = this.state;
+        const applesList = Array(apples).fill('🍎');
+
+        log('PARENT → render', 'red');
+
+        return (
+            <>
+                <br />
+                <div>
+                    Привет, фермер 👩🏼‍🌾 {name}. 👋🏼 У тебя {apples} яблок.
+                </div>
+                <br />
+                {applesList}
+                <br />
+                <br />
+                <button onClick = { this._yieldApples }>Собрать яблоки 🍎</button>
+                <Silo apples = { apples } />
             </>
         );
     }
